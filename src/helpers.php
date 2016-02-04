6a0440cb6784ae995634e70fc81636111f88b28c
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-02-04 12:05:12 -0200

    First Commit

--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,19 @@
 <?php
 
+use GuzzleHttp\Client;
+
 if (!function_exists('__')) {
     function __($key)
     {
+        $client = new Client(['base_uri' => getenv('TRANSLATE_URL')]);
+        $translate = $client->get('translate', ['query' => ['key' => $key]]);
 
+        if ($translate->getStatusCode() == 200) {
+            $message = json_decode($translate->getBody()->getContents());
+
+            if ($message->status) {
+                return $message->message;
+            }
+        }
     }
 }
