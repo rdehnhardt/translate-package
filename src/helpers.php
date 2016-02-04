dce7400e876af3503ddd77f1ecec8583f473eb35
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-02-04 13:53:58 -0200

    First Commit

--- src/helpers.php
+++ src/helpers.php
@@ -5,8 +5,10 @@ use GuzzleHttp\Client;
 if (!function_exists('__')) {
     function __($key)
     {
+        $locale = array_key_exists('locale', $_SESSION) ? $_SESSION['locale'] : getenv('TRANSLATE_LOCALE');
+
         $client = new Client(['base_uri' => getenv('TRANSLATE_URL')]);
-        $translate = $client->get('translate', ['query' => ['key' => $key]]);
+        $translate = $client->get('translate', ['query' => ['key' => $key, 'locale' => $locale]]);
 
         if ($translate->getStatusCode() == 200) {
             $message = json_decode($translate->getBody()->getContents());
@@ -14,6 +16,18 @@ if (!function_exists('__')) {
             if ($message->status) {
                 return $message->message;
             }
+        }
+    }
+}
+
+if (!function_exists('getLocales')) {
+    function getLocales()
+    {
+        $client = new Client(['base_uri' => getenv('TRANSLATE_URL')]);
+        $translate = $client->get('locales');
+
+        if ($translate->getStatusCode() == 200) {
+            return json_decode($translate->getBody()->getContents());
         }
     }
 }
